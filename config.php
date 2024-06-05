30db9478fb23430d66356a2a5e78e28d54712246
mikiyas <129675568+miki-babi@users.noreply.github.com>
2024-06-05 16:50:30 +0300

    Updated config.php

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
 
-$token="7292555524:AAEjg7yU6uYf46EQR7egRCAoPgZFZaraoAc";
+$token="";
 $webhookUrl = 'https://hooks.axumcode.com';
 $apiUrl = "https://api.telegram.org/bot$token/setWebhook";
